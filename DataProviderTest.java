b7f17b14b9da91d985a35df3a5288f674c611073
Elisa Huang <elhuang@paypal.com>
2015-07-13 23:40:50 -0700

    Revert "updating test"
    This reverts commit 4444b4fd553fc0b8a978c976934312d16934a123.

--- DataProviderTest.java
+++ DataProviderTest.java
@@ -1,12 +1,5 @@
 package com.paypal.test;
 
-import java.lang.reflect.Method;
-
-import org.testng.IInvokedMethod;
-import org.testng.ITestContext;
-import org.testng.ITestNGMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -17,30 +10,21 @@ import com.paypal.selion.platform.grid.Grid;
 @Test(singleThreaded = true)
 public class DataProviderTest {
 
-    @BeforeClass
-    public void isTestMethod(ITestContext context) {
-        ITestNGMethod[] methods = context.getAllTestMethods();
-        for (ITestNGMethod method : methods) {
-            System.out.println(method.getMethodName() + ": " + method.isTest());
-        }
-    }
-    
     @DataProvider(name = "dp")
-    @Test(priority = 0)
     public static Object[][] dp() {
-        return new Object[][] {{ "http://www.paypal.com" }, { "http://www.google.com" }}; 
+        return new Object[][] {{ "row1" }, { "row2" }}; 
     }
     
-    @Test(priority = 1)
+    @Test(priority = 0)
     public void meFirst() {
         System.out.println("first");
-        Grid.driver().get("http://www.facebook.com");
+        Grid.driver().get("http://www.paypal.com");
     }
     
-    @Test(priority = 2, dataProvider = "dp")
+    @Test(priority = 1, dependsOnMethods = "meFirst", dataProvider = "dp")
     public void test1(String row) {
         System.out.println(row);
-        Grid.driver().get(row);
+        Grid.driver().get("http://www.paypal.com");
     }
 }
 
