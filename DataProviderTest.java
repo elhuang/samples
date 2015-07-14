4444b4fd553fc0b8a978c976934312d16934a123
Elisa Huang <elhuang@paypal.com>
2015-07-13 23:40:02 -0700

    updating test

--- DataProviderTest.java
+++ DataProviderTest.java
@@ -1,5 +1,12 @@
 package com.paypal.test;
 
+import java.lang.reflect.Method;
+
+import org.testng.IInvokedMethod;
+import org.testng.ITestContext;
+import org.testng.ITestNGMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -10,21 +17,30 @@ import com.paypal.selion.platform.grid.Grid;
 @Test(singleThreaded = true)
 public class DataProviderTest {
 
-    @DataProvider(name = "dp")
-    public static Object[][] dp() {
-        return new Object[][] {{ "row1" }, { "row2" }}; 
+    @BeforeClass
+    public void isTestMethod(ITestContext context) {
+        ITestNGMethod[] methods = context.getAllTestMethods();
+        for (ITestNGMethod method : methods) {
+            System.out.println(method.getMethodName() + ": " + method.isTest());
+        }
     }
     
+    @DataProvider(name = "dp")
     @Test(priority = 0)
+    public static Object[][] dp() {
+        return new Object[][] {{ "http://www.paypal.com" }, { "http://www.google.com" }}; 
+    }
+    
+    @Test(priority = 1)
     public void meFirst() {
         System.out.println("first");
-        Grid.driver().get("http://www.paypal.com");
+        Grid.driver().get("http://www.facebook.com");
     }
     
-    @Test(priority = 1, dependsOnMethods = "meFirst", dataProvider = "dp")
+    @Test(priority = 2, dataProvider = "dp")
     public void test1(String row) {
         System.out.println(row);
-        Grid.driver().get("http://www.paypal.com");
+        Grid.driver().get(row);
     }
 }
 
